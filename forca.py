aeaf9af9e77fd318d765803c78fe3e89a6b01399
Pablo Vinícius Gonçalves Ramos <70920611+PabloGramos@users.noreply.github.com>
2020-11-19 13:48:47 -0300

    jogo da forca
    jogo da forca de a cordo com os exercicios do livro Python e orientação a objetos

---
+++ forca.py
@@ -0,0 +1,19 @@
+print("""
+-------------------Jogo da Forca-----------------
+
+""")
+palavra_chave='QUADRADO'
+acertou=False
+enforcou=False
+while(not acertou and not enforcou):
+    chute=input("Qual a letra? ")
+
+    posicao=0
+    for letra in palavra_chave:
+        if chute == letra:
+            print("Encontrou a letra {} na posição {}".format(letra,posicao))
+        posicao += 1
+        print("Jogando")
+
+
+print("Fim do jogo")
