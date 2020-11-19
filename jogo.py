55b1d153c29b6173fb72d579ccafd622c6c04be0
Pablo Vinícius Gonçalves Ramos <70920611+PabloGramos@users.noreply.github.com>
2020-11-19 13:16:51 -0300

    Update jogo.py

--- jogo.py
+++ jogo.py
@@ -2,15 +2,34 @@ def abertura():
     print("""
     --------------------Jogo da Adivinhação-----------------------
     
-    ----------------Adivinhe o numero de 0 a 10-------------------
+    -------------------Escolha a dificuldade----------------------
     """)
 
 n=7
-i=4
-tentativas=4
+i=0
+tentativas=0
 chute=0
 abertura()
-for tentativas in range(1,5):
+print("1-Facil")
+print("2-Normal")
+print("3-Dificil")
+nivel=int(input("-->"))
+if nivel == 1:
+    print("Adivinhe o numero de 0 a 100")
+    tentativas = 20
+    i = tentativas
+elif nivel == 2:
+    print("Adivinhe o numero de 0 a 100")
+    tentativas = 10
+    i = tentativas
+elif nivel == 3:
+    print("Adivinhe o numero de 0 a 200")
+    tentativas = 5
+    i = tentativas
+else:
+    print("Opção inválida!")
+    return
+for tentativas in range(1,i):
     print("Voce possui {} tentativas".format(i))
     number=int(input("Digite o valor...."))
     i -= 1
