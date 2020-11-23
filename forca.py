3f8db30d32eab0c7689c2403c45f77ac3214878f
Pablo Vinícius Gonçalves Ramos <70920611+PabloGramos@users.noreply.github.com>
2020-11-23 12:07:05 -0300

    continuaçao
    adicionando mais detalhes ao jogo

--- forca.py
+++ forca.py
@@ -2,18 +2,27 @@ print("""
 -------------------Jogo da Forca-----------------
 
 """)
-palavra_chave='QUADRADO'
+palavra_chave='BANANA'
 acertou=False
 enforcou=False
+erros=0
+letras_acertadas=['_','_','_','_','_','_']
+
+print(letras_acertadas)
 while(not acertou and not enforcou):
     chute=input("Qual a letra? ")
-
-    posicao=0
-    for letra in palavra_chave:
-        if chute.upper() == letra.upper():
-            print("Encontrou a letra {} na posição {}".format(letra,posicao))
-        posicao += 1
-    print("Jogando")
+    if chute in palavra_chave:
+        posicao=0
+        for letra in palavra_chave:
+            if chute.upper() == letra.upper():
+                print("Encontrou a letra {} na posição {}".format(letra,posicao))
+                letras_acertadas[posicao]=letra
+            posicao += 1
+        acertou = '_' not in letras_acertadas
+        enforcou = erros == 6
+    else:
+        erros += 1
+    print(letras_acertadas)
 
 
 print("Fim do jogo")
