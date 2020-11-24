74ac3f632c54862c2d2386e15bd07b79773c901f
Pablo Vinícius Gonçalves Ramos <70920611+PabloGramos@users.noreply.github.com>
2020-11-24 18:52:10 -0300

    Arquivo para anotaçoes e exercicios da apostila da Caelum
    Criei um arquivo para anotar oq aprendi na apostila

---
+++ anotaçoes_caelum.py
@@ -0,0 +1,36 @@
+a = set('abacate')
+b = set('abacaxi')
+
+print(a)
+print(b)
+
+print(a-b,'diferença')
+print(a|b,'união')
+print(a&b,'intercessão')
+print(a^b,'diferença simetrica')
+
+# DICIONARIOS
+
+pessoa={'nome':'Joao','idade':25,'cidade':'São Paulo'}
+print(pessoa['nome'])
+print(pessoa['idade'])
+pessoa['pais'] = 'Brasil'
+print(pessoa['pais'])
+print(pessoa.keys())
+print(pessoa.values())
+
+
+lista=[12,	-2,	4,	8,	29,	45,	78,	36,	-17,	2,	12,	8,	3,	3,	-52]
+
+print(max(lista),'minimo')
+print(min(lista),'maximo')
+# entender ocorrencia
+n=0
+media=0
+i=0
+while i <= len(lista):
+    print(lista[i])
+    n = n + lista[i]
+    i += 1
+media = n / len(lista)
+print('a media da lista é ',media)
