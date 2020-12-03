f487bddb86875c0709f77991ee0544d53dc47c50
Pablo Vinícius Gonçalves Ramos <70920611+PabloGramos@users.noreply.github.com>
2020-12-02 22:02:26 -0300

    Atualizando o codigo de teste
    teste do codigo sobre def dentro de uma class ou objeto

--- conta_OO.py
+++ conta_OO.py
@@ -1,11 +1,25 @@
 class Conta:
     def __init__(self,numero,titular,saldo,limite):
+        print('Iniciando a conta')
         self.numero=numero
         self.titular=titular
         self.saldo=saldo
         self.limite=limite
 
-    pass
+    def deposita(self, valor):
+        self.saldo += valor
 
-from conta import Conta
-conta = Conta()
+    def saca(self,valor):
+        self.saldo -= valor
+        return True
+
+    def extrato(self):
+        print("Numero: {}\nExtrato: {}".format(self.numero,self.saldo))
+
+conta = Conta('123-4','João',120.0,1000.0)
+print("{}\n{}\n{}\n{}".format(conta.numero,conta.titular,conta.saldo,conta.limite))
+conta.deposita(20.0)
+print("Novo saldo apos deposito: ",conta.saldo)
+conta.saca(30.0)
+print("Novo saldo apos saque: ",conta.saldo)
+conta.extrato()
