b9ce50d999638fa9bd3e56bc27a1f3f277bdfca4
Pablo Vinícius Gonçalves Ramos <70920611+PabloGramos@users.noreply.github.com>
2020-12-06 13:48:12 -0300

    implementando novas classes
    adicionei novas classes e a junçao entre duas classes

--- conta_OO.py
+++ conta_OO.py
@@ -1,30 +1,61 @@
+import datetime
+class Historico:
+    def __init__(self):
+        self.data_abertura = datetime.datetime.today()
+        self.transacoes = []
+
+    def imprime(self):
+        print("data abertura: {}".format(self.data_abertura))
+        print("transações: ")
+        for t in self.transacoes:
+            print("-",t)
+
+class Cliente:
+    def __init__(self,nome,sobrenome,cpf):
+        self.nome = nome
+        self.sobrenome = sobrenome
+        self.cpf = cpf
+
 class Conta:
-    def __init__(self,numero,titular,saldo,limite):
+    def __init__(self,numero,titular,saldo,limite=1000.0):
         print('Iniciando a conta')
         self.numero=numero
         self.titular=titular
         self.saldo=saldo
         self.limite=limite
+        self.historico = Historico()
 
     def deposita(self, valor):
         self.saldo += valor
+        self.historico.transacoes.append("deposito de {}".format(valor))
 
     def saca(self,valor):
-        self.saldo -= valor
-        return True
+        if (self.saldo < valor):
+            return False
+        else:
+            self.saldo -= valor
+            self.historico.transacoes.append("saque de {}".format(valor))
 
     def extrato(self):
         print("Numero: {}\nExtrato: {}".format(self.numero,self.saldo))
+        self.historico.transacoes.append("tirou extrato de {}".format(self.saldo))
 
     def transfere(self, destino, valor):
-        self.saldo -= valor
-        destino.saldo += valor
+        retirou = self.saca(valor)
+        if (retirou == False):
+            return False
+        else:
+            destino.deposita(valor)
+            self.historico.transacoes.append("transferencia de {} para conta {}".format(valor, destino.numero))
+            return True
 
-
-conta = Conta('123-4','João',120.0,1000.0)
+cliente = Cliente('Joao', 'Oliveira', '1111111111-1')
+minha_conta = Conta('123-4', cliente, 120.0, 1000.0)
+conta = Conta('123-4','João',120.0)
 print("{}\n{}\n{}\n{}".format(conta.numero,conta.titular,conta.saldo,conta.limite))
 conta.deposita(20.0)
 print("Novo saldo apos deposito: ",conta.saldo)
 conta.saca(30.0)
 print("Novo saldo apos saque: ",conta.saldo)
 conta.extrato()
+print("Transderido R$300.00")
