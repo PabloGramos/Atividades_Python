70b745a9d80d99654034a69331f47b2a617acd66
Pablo Vinícius Gonçalves Ramos <70920611+PabloGramos@users.noreply.github.com>
2020-12-16 16:41:57 -0300

    atualizando o codigo
    verificando o efeito da herança dentro da classe

--- conta.py
+++ conta.py
@@ -4,9 +4,12 @@ class Funcionario:
         self._cpf = cpf
         self._salario = salario
 
+    def get_bonificacao(self):
+        return self._salario * 0.10
+
 class Gerente(Funcionario):
-    def __init__(self, senha, qtd_gerenciados):
-        Funcionario.__init__(nome,cpf,salario)
+    def __init__(self,nome,cpf,salario, senha, qtd_gerenciados):
+        super().__init__(nome,cpf,salario)
         self._senha = senha
         self._qtd_gerenciados = qtd_gerenciados
 
@@ -17,3 +20,14 @@ class Gerente(Funcionario):
         else:
             print("acesso negado")
             return False
+    def get_bonificacao(self):
+        return self._salario * 0.15
+
+
+gerente = Gerente('Jose','222222222-22',5000.0,'1234',0)
+print(gerente.get_bonificacao())
+
+funcionario = Funcionario('joao','111111111-11',2000.0)
+print(funcionario.get_bonificacao())
+print(vars(funcionario))
+print(vars(gerente))
